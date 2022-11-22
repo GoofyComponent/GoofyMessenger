f74e883c1b5d9f417b025c2e8cfbde566ad906f0
lucalg322 <95612509+lucalg322@users.noreply.github.com>
2022-11-22 17:58:49 +0100

    add component and css

---
+++ frontend/src/component/sideBar/sideBar.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Row, Container, Col, Button } from "react-bootstrap";
+import "../../css/sidebar.css";
+
+function sidebar() {
+  return (
+    <>
+      <h1>ptit test</h1>
+    </>
+  );
+}
+
+export default sidebar;
