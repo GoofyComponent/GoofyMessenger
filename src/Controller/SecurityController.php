a9c5ac9f0188a3db033d2b0917688a3ccbcf0ee8
Adrien Albuquerque <adrialb95@gmail.com>
2022-11-22 21:23:13 +0100

    auth JWT + Factorie

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Service\CookieHelper;
+use App\Service\JWTHelper;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(JWTHelper $helper, CookieHelper $cookieHelper): Response
+    {
+        /** @var $user User */
+        if ($user = $this->getUser()) {
+            return $this->json([
+                'JWT' => $helper->createJWT($user),
+                'status' => 'success',
+            ], 200, [
+                'set-cookie' => $cookieHelper->createMercureCookie($user)
+            ]);
+        }
+
+        return $this->json([
+            'status' => 'error',
+            'message' => 'Bad credentials',
+            'Authorization' => 'Basic'
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
