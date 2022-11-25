c60a135d4e8ea1a92a37ea924ad26eb7402c3726
lucalg322 <95612509+lucalg322@users.noreply.github.com>
2022-11-25 09:41:21 +0100

    bottom bare

---
+++ frontend/src/component/BottomBar/BottomBar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../css/bottombar.css";
+
+function BottomBar() {
+  return (
+    <div className="Row bottomBar">
+      <div className="bottomBox">
+        <textarea name="Message" id="Message"></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default BottomBar;
--- frontend/src/component/Home/home.tsx
+++ frontend/src/component/Home/home.tsx
@@ -5,11 +5,13 @@ import { Form, Row, Container, Col, Button } from "react-bootstrap";
 import axios from "axios";
 import "../../css/login.css";
 import Sidebar from "../SideBar/sideBar";
+import BottomBar from "../BottomBar/BottomBar";
 
 function Home() {
   return (
     <>
       <Sidebar></Sidebar>
+      <BottomBar></BottomBar>
     </>
   );
 }
