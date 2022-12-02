d6e244e6cb485824784a11dea696ab8e871775c8
Adrien Albuquerque <adrialb95@gmail.com>
2022-12-02 16:38:12 +0100

    Mise en place de la Home

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import  HomeScreen  from './components/HomeScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
